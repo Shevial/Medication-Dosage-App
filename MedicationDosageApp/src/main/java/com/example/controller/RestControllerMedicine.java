2320b7cafba11af6ca9281ee70e4e5eb3cda4487
Sara HL <sarahevialopez@gmail.com>
2025-02-12 18:01:31 +0100

    Css

--- MedicationDosageApp/src/main/java/com/example/controller/RestControllerMedicine.java
+++ MedicationDosageApp/src/main/java/com/example/controller/RestControllerMedicine.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/medicines")
 public class RestControllerMedicine {

d8a103f80e8eaf78381f6b5d752d2fc55dc67557
Sara HL <sarahevialopez@gmail.com>
2025-03-20 14:06:10 +0100

    Fixed Dotenv

--- MedicationDosageApp/src/main/java/com/example/MainApp.java
+++ MedicationDosageApp/src/main/java/com/example/MainApp.java
@@ -1,12 +1,21 @@
 package com.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApp {
 
+
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        // Establecer cada variable en las propiedades del sistema para que Spring Boot las encuentre
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(MainApp.class, args);
     }
 
